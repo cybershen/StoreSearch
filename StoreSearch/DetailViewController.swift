03fe071f46c94bb66236617c1e4a453ad51e3619
Nazar <cyberpuer@proton.me>
2022-09-08 13:22:04 +0300

    Add DetailViewController and the code to show/hide the pop-up. Complete the design for the Detail pop-up. Show data in the pop-up.

---
+++ StoreSearch/DetailViewController.swift
@@ -0,0 +1,98 @@
+//
+//  DetailViewController.swift
+//  StoreSearch
+//
+//  Created by Назар Жиленко on 08.09.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
+    
+    var searchResult: SearchResult!
+    var downloadTask: URLSessionDownloadTask?
+    
+    deinit {
+        downloadTask?.cancel()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        popupView.layer.cornerRadius = 10
+        
+        let gestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(close))
+        gestureRecognizer.cancelsTouchesInView = false
+        gestureRecognizer.delegate = self
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        if searchResult != nil {
+            updateUI()
+        }
+    }
+    
+    // MARK: - Actions
+    @IBAction func openInStore() {
+        if let url = URL(string: searchResult.storeURL) {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    // MARK: - Helper Methods
+    func updateUI() {
+        nameLabel.text = searchResult.name
+        
+        if searchResult.artist.isEmpty {
+            artistNameLabel.text = "Unknown"
+        } else {
+            artistNameLabel.text = searchResult.artist
+        }
+        
+        kindLabel.text = searchResult.type
+        genreLabel.text = searchResult.genre
+        
+        // Show price
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = searchResult.currency
+        
+        let priceText: String
+        
+        if searchResult.price == 0 {
+            priceText = "Free"
+        } else if let text = formatter.string(from: searchResult.price as NSNumber) {
+            priceText = text
+        } else {
+            priceText = ""
+        }
+        
+        priceButton.setTitle(priceText, for: .normal)
+        
+        // Get image
+        if let largeURL = URL(string: searchResult.imageLarge) {
+            downloadTask = artworkImageView.loadImage(url: largeURL)
+        }
+    }
+    
+    
+    @objc func close() {
+        dismiss(animated: true)
+    }
+}
+
+// MARK: - Gesture Recognizer Delegate
+extension DetailViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return (touch.view === self.view)
+    }
+}
--- StoreSearch/UIImageView+DownloadImage.swift..swift
+++ StoreSearch/UIImageView+DownloadImage.swift..swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UIImageView {
+    
     func loadImage(url: URL) -> URLSessionDownloadTask {
         let session = URLSession.shared
         let downloadTask = session.downloadTask(with: url) {
